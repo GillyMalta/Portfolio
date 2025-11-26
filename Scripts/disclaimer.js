014f632a12d514f7fe0df9a2a29fe15a21a6343f
Gilbert_Sammut <gilbert@79297110.com>
2025-11-26 09:12:06 +0100

    commited

--- Scripts/disclaimer.js
+++ Scripts/disclaimer.js
@@ -1,6 +1,6 @@
 // headers.js
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("/Pages/Footers/disclaimer.html")
+  fetch("Pages/Footers/disclaimer.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById("disclaimer_Placeholder").innerHTML = data;
